f9434e2c0d6a1021f404fcfcb38b3511d6f2157c
Chad Scribner <brandisher@users.noreply.github.com>
2020-04-03 12:52:11 -0400

    Add 5s_delay enpoint

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-
 from flask import Flask
+from time import sleep
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/5s_delay')
+def slow_startup():
+    sleep(5)
+    return "5 second delay"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
